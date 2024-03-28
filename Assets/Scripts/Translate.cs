8f32136a0fe57630016a91a01b3c2933963d2eee
Jad-Kreit29 <84517030+Jad-Kreit29@users.noreply.github.com>
2024-03-27 22:38:06 -0400

    Implementing Morse Decoder

--- Assets/Scripts/Translate.cs
+++ Assets/Scripts/Translate.cs
@@ -1,20 +1,57 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Translate : MonoBehaviour
 {
+    private String message;
 
-    [SerializeField] public TMP_InputField messageInput;
-    [SerializeField] public TextMeshProUGUI messageOutput;
+    public TMP_InputField messageInput;
+    
+    public TMP_Text messageOutput;
     
     public void TranslateMorse() {
-        if (messageInput.text == null) {
+        
+        Debug.Log("You've pressed me!");
+
+        if (messageInput.text == "") {
             messageOutput.text = "You have given me NOTHING!";
+        } else if (!messageInput.text.Contains("-") && !messageInput.text.Contains(".")) {
+            messageOutput.text = "I can't work with this!";
+        } else {
+
+            String[] messageBits = messageInput.text.Split(' ');
+
+            for (int i = 0; i < messageBits.Length; i++) {
+                switch (messageBits[i])
+                {
+                    case ".-":
+                        message += "A";
+                        break;
+                    case "-...":
+                        message += "B";
+                        break;
+                    case "-.-.":
+                        message += "C";
+                        break;
+                    case "-..":
+                        message += "D";
+                        break;
+                    case ".":
+                        message += "E";
+                        break;
+                    
+                }
+            }
+
+            messageOutput.text = message;
         }
+
+
     }
 
 }
